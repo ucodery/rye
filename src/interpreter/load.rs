5573d8e24e5b735249cfcd939909c93363b5b734
Jeremy Paige <ucodery@gmail.com>
2021-07-08 22:20:22 -0700

    reset to ground zero

--- src/interpreter/load.rs
+++
@@ -1,7 +0,0 @@
-pub fn load_from_file(path: String) {
-    let byte_file = std::fs::read(path).expect("Nothing in the file");
-
-    for byte_code in byte_file {
-        println!("Got: {:X}", byte_code)
-    }
-}
