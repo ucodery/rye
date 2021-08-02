3392aa9b254a5a6064d2bec5aca29e75b61276f3
Jeremy Paige <ucodery@gmail.com>
2021-08-02 14:39:37 -0700

    chagne Token struct

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,31 +1,26 @@
-use std::cmp;
-
-use crate::tokens;
+use crate::tokens::{get_op, Token, TokenType};
 
 use unicode_categories::UnicodeCategories;
 
-pub struct TokenStream<'a> {
-    input: &'a str,
-    input_end: usize,
+pub struct TokenStream {
+    input: Vec<char>,
     cursor: usize,
 }
 
-impl TokenStream<'_> {
+impl TokenStream {
     pub fn new(input: &str) -> TokenStream {
         TokenStream {
-            input,
-            input_end: input.chars().count(),
+            input: input.chars().collect(),
             cursor: 0,
         }
     }
 
     /// Attempt to consume the longest valid op token under the cursor
     /// advance the cursor if any op is found
-    fn consume_next_op_token(&mut self) -> Option<tokens::Token> {
-        let largest_window = cmp::min(self.input_end - self.cursor, 3);
-        let tok = tokens::get_op(&self.input[self.cursor..self.cursor + largest_window]);
+    fn consume_next_op_token(&mut self) -> Option<Token> {
+        let tok = get_op(&self.input[self.cursor..]);
         match tok {
-            Some(tokens::Token {
+            Some(Token {
                 token_type: _,
                 size: Some(size),
             }) => self.cursor += size,
@@ -34,8 +29,15 @@ impl TokenStream<'_> {
         tok
     }
 
-    fn is_start_of_name(c: char) -> bool {
-        if c.is_letter_uppercase() || c.is_letter_lowercase() || c.is_letter_titlecase() || c.is_letter_modifier() || c.is_letter_other() || c.is_number_letter() || c == '_'{
+    fn is_start_of_name(c: &char) -> bool {
+        if c.is_letter_uppercase()
+            || c.is_letter_lowercase()
+            || c.is_letter_titlecase()
+            || c.is_letter_modifier()
+            || c.is_letter_other()
+            || c.is_number_letter()
+            || *c == '_'
+        {
             // XXX: also chars with Other_ID_Start property
             true
         } else {
@@ -43,8 +45,13 @@ impl TokenStream<'_> {
         }
     }
 
-    fn is_part_of_name(c: char) -> bool {
-        if Self::is_start_of_name(c) || c.is_mark_nonspacing() || c.is_mark_spacing_combining() || c.is_number_decimal_digit() || c.is_punctuation_connector() {
+    fn is_part_of_name(c: &char) -> bool {
+        if Self::is_start_of_name(c)
+            || c.is_mark_nonspacing()
+            || c.is_mark_spacing_combining()
+            || c.is_number_decimal_digit()
+            || c.is_punctuation_connector()
+        {
             // XXX: also chars with Other_ID_Continue
             true
         } else {
@@ -52,46 +59,55 @@ impl TokenStream<'_> {
         }
     }
 
-    fn consume_next_name_token(&mut self) -> Option<tokens::Token> {
-        let next_char = self.input[self.cursor..self.cursor+1].chars().next().unwrap();
+    fn consume_next_name_token(&mut self) -> Option<Token> {
+        let next_char = self.input[self.cursor..=self.cursor].first().unwrap();
         if !Self::is_start_of_name(next_char) {
-            return None
+            return None;
         }
 
         let mut name_end = self.cursor + 1;
-        while Self::is_part_of_name(self.input[name_end..name_end+1].chars().next().unwrap()) {
+        while Self::is_part_of_name(self.input[name_end..=name_end].first().unwrap()) {
             name_end += 1;
         }
-        let tok = Some(tokens::Token {token_type: tokens::TokenType::NAME, size: Some(name_end - self.cursor)});
+        let tok = Some(Token {
+            token_type: TokenType::NAME,
+            size: Some(name_end - self.cursor),
+        });
         self.cursor = name_end;
         tok
     }
 }
 
-impl Iterator for TokenStream<'_> {
-    type Item = tokens::Token;
+impl Iterator for TokenStream {
+    type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             // check for end
-            if self.cursor > self.input_end {
-                return None
+            if self.cursor > self.input.len() {
+                return None;
             }
-            if self.cursor == self.input_end {
+            if self.cursor == self.input.len() {
                 self.cursor += 1;
-                return Some(tokens::Token {token_type: tokens::TokenType::ENDMARKER, size: None})
+                return Some(Token {
+                    token_type: TokenType::ENDMARKER,
+                    size: None,
+                });
             }
 
             if let Some(tok) = self.consume_next_op_token() {
-                return Some(tok)
+                return Some(tok);
             };
             if let Some(tok) = self.consume_next_name_token() {
-                return Some(tok)
+                return Some(tok);
             };
 
             // no tokens found
             self.cursor += 1;
-            return Some(tokens::Token {token_type: tokens::TokenType::ERRORTOKEN, size: None})
+            return Some(Token {
+                token_type: TokenType::ERRORTOKEN,
+                size: None,
+            });
         }
     }
 }
--- src/tokens.rs
+++ src/tokens.rs
@@ -72,204 +72,200 @@ pub struct Token {
     pub size: Option<usize>,
 }
 
-pub fn get_op(c1: &str) -> Option<Token> {
-    match c1.get(..3) {
-        Some("**=") => Some(Token {
+pub fn get_op(c1: &[char]) -> Option<Token> {
+    match c1 {
+        ['*', '*', '=', ..] => Some(Token {
             token_type: TokenType::DOUBLESTAREQUAL,
             size: Some("**=".chars().count()),
         }),
-        Some("...") => Some(Token {
+        ['.', '.', '.', ..] => Some(Token {
             token_type: TokenType::ELLIPSIS,
             size: Some("...".chars().count()),
         }),
-        Some("//=") => Some(Token {
+        ['/', '/', '=', ..] => Some(Token {
             token_type: TokenType::DOUBLESLASHEQUAL,
             size: Some("//=".chars().count()),
         }),
-        Some("<<=") => Some(Token {
+        ['<', '<', '=', ..] => Some(Token {
             token_type: TokenType::LEFTSHIFTEQUAL,
             size: Some("<<=".chars().count()),
         }),
-        Some(">>=") => Some(Token {
+        ['>', '>', '=', ..] => Some(Token {
             token_type: TokenType::RIGHTSHIFTEQUAL,
             size: Some(">>=".chars().count()),
         }),
-        _ => match c1.get(..2) {
-            Some("!=") => Some(Token {
-                token_type: TokenType::NOTEQUAL,
-                size: Some("!=".chars().count()),
-            }),
-            Some("%=") => Some(Token {
-                token_type: TokenType::PERCENTEQUAL,
-                size: Some("%=".chars().count()),
-            }),
-            Some("&=") => Some(Token {
-                token_type: TokenType::AMPEREQUAL,
-                size: Some("&=".chars().count()),
-            }),
-            Some("**") => Some(Token {
-                token_type: TokenType::DOUBLESTAR,
-                size: Some("**".chars().count()),
-            }),
-            Some("*=") => Some(Token {
-                token_type: TokenType::STAREQUAL,
-                size: Some("*=".chars().count()),
-            }),
-            Some("+=") => Some(Token {
-                token_type: TokenType::PLUSEQUAL,
-                size: Some("+=".chars().count()),
-            }),
-            Some("-=") => Some(Token {
-                token_type: TokenType::MINEQUAL,
-                size: Some("-=".chars().count()),
-            }),
-            Some("->") => Some(Token {
-                token_type: TokenType::RARROW,
-                size: Some("->".chars().count()),
-            }),
-            Some("//") => Some(Token {
-                token_type: TokenType::DOUBLESLASH,
-                size: Some("//".chars().count()),
-            }),
-            Some("/=") => Some(Token {
-                token_type: TokenType::SLASHEQUAL,
-                size: Some("/=".chars().count()),
-            }),
-            Some(":=") => Some(Token {
-                token_type: TokenType::COLONEQUAL,
-                size: Some(":=".chars().count()),
-            }),
-            Some("<<") => Some(Token {
-                token_type: TokenType::LEFTSHIFT,
-                size: Some("<<".chars().count()),
-            }),
-            Some("<=") => Some(Token {
-                token_type: TokenType::LESSEQUAL,
-                size: Some("<=".chars().count()),
-            }),
-            Some("<>") => Some(Token {
-                token_type: TokenType::NOTEQUAL,
-                size: Some("<>".chars().count()),
-            }),
-            Some("==") => Some(Token {
-                token_type: TokenType::EQEQUAL,
-                size: Some("==".chars().count()),
-            }),
-            Some(">=") => Some(Token {
-                token_type: TokenType::GREATEREQUAL,
-                size: Some(">=".chars().count()),
-            }),
-            Some(">>") => Some(Token {
-                token_type: TokenType::RIGHTSHIFT,
-                size: Some(">>".chars().count()),
-            }),
-            Some("@=") => Some(Token {
-                token_type: TokenType::ATEQUAL,
-                size: Some("@=".chars().count()),
-            }),
-            Some("^=") => Some(Token {
-                token_type: TokenType::CIRCUMFLEXEQUAL,
-                size: Some("^=".chars().count()),
-            }),
-            Some("|=") => Some(Token {
-                token_type: TokenType::VBAREQUAL,
-                size: Some("|=".chars().count()),
-            }),
-            _ => match c1.get(..1) {
-                Some("%") => Some(Token {
-                    token_type: TokenType::PERCENT,
-                    size: Some("%".chars().count()),
-                }),
-                Some("&") => Some(Token {
-                    token_type: TokenType::AMPER,
-                    size: Some("&".chars().count()),
-                }),
-                Some("(") => Some(Token {
-                    token_type: TokenType::LPAR,
-                    size: Some("(".chars().count()),
-                }),
-                Some(")") => Some(Token {
-                    token_type: TokenType::RPAR,
-                    size: Some(")".chars().count()),
-                }),
-                Some("*") => Some(Token {
-                    token_type: TokenType::STAR,
-                    size: Some("*".chars().count()),
-                }),
-                Some("+") => Some(Token {
-                    token_type: TokenType::PLUS,
-                    size: Some("+".chars().count()),
-                }),
-                Some(",") => Some(Token {
-                    token_type: TokenType::COMMA,
-                    size: Some(",".chars().count()),
-                }),
-                Some("-") => Some(Token {
-                    token_type: TokenType::MINUS,
-                    size: Some("-".chars().count()),
-                }),
-                Some(".") => Some(Token {
-                    token_type: TokenType::DOT,
-                    size: Some(".".chars().count()),
-                }),
-                Some("/") => Some(Token {
-                    token_type: TokenType::SLASH,
-                    size: Some("/".chars().count()),
-                }),
-                Some(":") => Some(Token {
-                    token_type: TokenType::COLON,
-                    size: Some(":".chars().count()),
-                }),
-                Some(";") => Some(Token {
-                    token_type: TokenType::SEMI,
-                    size: Some(";".chars().count()),
-                }),
-                Some("<") => Some(Token {
-                    token_type: TokenType::LESS,
-                    size: Some("<".chars().count()),
-                }),
-                Some("=") => Some(Token {
-                    token_type: TokenType::EQUAL,
-                    size: Some("=".chars().count()),
-                }),
-                Some(">") => Some(Token {
-                    token_type: TokenType::GREATER,
-                    size: Some(">".chars().count()),
-                }),
-                Some("@") => Some(Token {
-                    token_type: TokenType::AT,
-                    size: Some("@".chars().count()),
-                }),
-                Some("[") => Some(Token {
-                    token_type: TokenType::LSQB,
-                    size: Some("[".chars().count()),
-                }),
-                Some("]") => Some(Token {
-                    token_type: TokenType::RSQB,
-                    size: Some("]".chars().count()),
-                }),
-                Some("^") => Some(Token {
-                    token_type: TokenType::CIRCUMFLEX,
-                    size: Some("^".chars().count()),
-                }),
-                Some("{") => Some(Token {
-                    token_type: TokenType::LBRACE,
-                    size: Some("{".chars().count()),
-                }),
-                Some("|") => Some(Token {
-                    token_type: TokenType::VBAR,
-                    size: Some("|".chars().count()),
-                }),
-                Some("}") => Some(Token {
-                    token_type: TokenType::RBRACE,
-                    size: Some("}".chars().count()),
-                }),
-                Some("~") => Some(Token {
-                    token_type: TokenType::TILDE,
-                    size: Some("~".chars().count()),
-                }),
-                _ => None,
-            },
-        },
+        ['!', '=', ..] => Some(Token {
+            token_type: TokenType::NOTEQUAL,
+            size: Some("!=".chars().count()),
+        }),
+        ['%', '=', ..] => Some(Token {
+            token_type: TokenType::PERCENTEQUAL,
+            size: Some("%=".chars().count()),
+        }),
+        ['&', '=', ..] => Some(Token {
+            token_type: TokenType::AMPEREQUAL,
+            size: Some("&=".chars().count()),
+        }),
+        ['*', '*', ..] => Some(Token {
+            token_type: TokenType::DOUBLESTAR,
+            size: Some("**".chars().count()),
+        }),
+        ['*', '=', ..] => Some(Token {
+            token_type: TokenType::STAREQUAL,
+            size: Some("*=".chars().count()),
+        }),
+        ['+', '=', ..] => Some(Token {
+            token_type: TokenType::PLUSEQUAL,
+            size: Some("+=".chars().count()),
+        }),
+        ['-', '=', ..] => Some(Token {
+            token_type: TokenType::MINEQUAL,
+            size: Some("-=".chars().count()),
+        }),
+        ['-', '>', ..] => Some(Token {
+            token_type: TokenType::RARROW,
+            size: Some("->".chars().count()),
+        }),
+        ['/', '/', ..] => Some(Token {
+            token_type: TokenType::DOUBLESLASH,
+            size: Some("//".chars().count()),
+        }),
+        ['/', '=', ..] => Some(Token {
+            token_type: TokenType::SLASHEQUAL,
+            size: Some("/=".chars().count()),
+        }),
+        [':', '=', ..] => Some(Token {
+            token_type: TokenType::COLONEQUAL,
+            size: Some(":=".chars().count()),
+        }),
+        ['<', '<', ..] => Some(Token {
+            token_type: TokenType::LEFTSHIFT,
+            size: Some("<<".chars().count()),
+        }),
+        ['<', '=', ..] => Some(Token {
+            token_type: TokenType::LESSEQUAL,
+            size: Some("<=".chars().count()),
+        }),
+        ['<', '>', ..] => Some(Token {
+            token_type: TokenType::NOTEQUAL,
+            size: Some("<>".chars().count()),
+        }),
+        ['=', '=', ..] => Some(Token {
+            token_type: TokenType::EQEQUAL,
+            size: Some("==".chars().count()),
+        }),
+        ['>', '=', ..] => Some(Token {
+            token_type: TokenType::GREATEREQUAL,
+            size: Some(">=".chars().count()),
+        }),
+        ['>', '>', ..] => Some(Token {
+            token_type: TokenType::RIGHTSHIFT,
+            size: Some(">>".chars().count()),
+        }),
+        ['@', '=', ..] => Some(Token {
+            token_type: TokenType::ATEQUAL,
+            size: Some("@=".chars().count()),
+        }),
+        ['^', '=', ..] => Some(Token {
+            token_type: TokenType::CIRCUMFLEXEQUAL,
+            size: Some("^=".chars().count()),
+        }),
+        ['|', '=', ..] => Some(Token {
+            token_type: TokenType::VBAREQUAL,
+            size: Some("|=".chars().count()),
+        }),
+        ['%', ..] => Some(Token {
+            token_type: TokenType::PERCENT,
+            size: Some("%".chars().count()),
+        }),
+        ['&', ..] => Some(Token {
+            token_type: TokenType::AMPER,
+            size: Some("&".chars().count()),
+        }),
+        ['(', ..] => Some(Token {
+            token_type: TokenType::LPAR,
+            size: Some("(".chars().count()),
+        }),
+        [')', ..] => Some(Token {
+            token_type: TokenType::RPAR,
+            size: Some(")".chars().count()),
+        }),
+        ['*', ..] => Some(Token {
+            token_type: TokenType::STAR,
+            size: Some("*".chars().count()),
+        }),
+        ['+', ..] => Some(Token {
+            token_type: TokenType::PLUS,
+            size: Some("+".chars().count()),
+        }),
+        [',', ..] => Some(Token {
+            token_type: TokenType::COMMA,
+            size: Some(",".chars().count()),
+        }),
+        ['-', ..] => Some(Token {
+            token_type: TokenType::MINUS,
+            size: Some("-".chars().count()),
+        }),
+        ['.', ..] => Some(Token {
+            token_type: TokenType::DOT,
+            size: Some(".".chars().count()),
+        }),
+        ['/', ..] => Some(Token {
+            token_type: TokenType::SLASH,
+            size: Some("/".chars().count()),
+        }),
+        [':', ..] => Some(Token {
+            token_type: TokenType::COLON,
+            size: Some(":".chars().count()),
+        }),
+        [';', ..] => Some(Token {
+            token_type: TokenType::SEMI,
+            size: Some(";".chars().count()),
+        }),
+        ['<', ..] => Some(Token {
+            token_type: TokenType::LESS,
+            size: Some("<".chars().count()),
+        }),
+        ['=', ..] => Some(Token {
+            token_type: TokenType::EQUAL,
+            size: Some("=".chars().count()),
+        }),
+        ['>', ..] => Some(Token {
+            token_type: TokenType::GREATER,
+            size: Some(">".chars().count()),
+        }),
+        ['@', ..] => Some(Token {
+            token_type: TokenType::AT,
+            size: Some("@".chars().count()),
+        }),
+        ['[', ..] => Some(Token {
+            token_type: TokenType::LSQB,
+            size: Some("[".chars().count()),
+        }),
+        [']', ..] => Some(Token {
+            token_type: TokenType::RSQB,
+            size: Some("]".chars().count()),
+        }),
+        ['^', ..] => Some(Token {
+            token_type: TokenType::CIRCUMFLEX,
+            size: Some("^".chars().count()),
+        }),
+        ['{', ..] => Some(Token {
+            token_type: TokenType::LBRACE,
+            size: Some("{".chars().count()),
+        }),
+        ['|', ..] => Some(Token {
+            token_type: TokenType::VBAR,
+            size: Some("|".chars().count()),
+        }),
+        ['}', ..] => Some(Token {
+            token_type: TokenType::RBRACE,
+            size: Some("}".chars().count()),
+        }),
+        ['~', ..] => Some(Token {
+            token_type: TokenType::TILDE,
+            size: Some("~".chars().count()),
+        }),
+        _ => None,
     }
 }
